a68a7fc5d03be6783fa8147be0b9175d00299e7f
Art3mis09352 <shalaevalex@yandex.ru>
2025-01-23 02:08:09 +0300

    34242

--- avtomat/Program.cs
+++ avtomat/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.SqlClient;
 using System.Linq;
-using System.Runtime.Remoting.Contexts;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,42 +11,12 @@ namespace avtomat
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-      
+        [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            Database.SetInitializer<Context>(null);
-            //using (Context DB = new Context())
-            //{
-
-            //    TGROUP test = new TGROUP
-            //    {
-            //        Id = 8,
-            //        Name = "Alex"
-            //    };
-            //    DB.TGroup.Add(test);
-
-
-            //    // Добавляем новые отношения в таблицу TRELATION (предполагаем, что IdParent и IdChild существуют)
-            //    var newRelations = new[]
-            //    {
-            //        new TRELATION { IdParent = 1, IdChild = 4 },  // Связи с существующими группами
-            //        new TRELATION { IdParent = 1, IdChild = 5 },
-            //        new TRELATION { IdParent = 3, IdChild = 8 }
-            //    };
-
-            //    DB.TRelation.AddRange(newRelations);
-
-            //    // Сохраняем изменения в базе данных
-            //    DB.SaveChanges();
-            //}
-
-
         }
-
-
     }
-    
 }
